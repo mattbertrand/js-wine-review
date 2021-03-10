e3de5bb62da971bdd2806ccaee082da441e28d99
github username <github email address>
2021-03-10 13:14:40 -0800

    added a formTemplate function to index.js

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,3 +5,25 @@ function main() {
 function resetMain() {
     main().innerHTML = "";
 }
+
+function formTemplate() {
+    return `
+    <h3>Add Wine</h3>
+        <form id="form">
+            <div class="input-field">
+                <label for="name">Wine Appellation</label>
+                <input type="text" name="name" id="name">
+            </div>
+            <div class="input-field">
+                <label for="varietal">Grape varietal</label>
+                <input type="text" name="varietal" id="varietal">
+            </div>
+            <div class="input-field">
+                <label for="vintage">Grape vintage</label>
+                <input type="integer" name="vintage" id="vintage">
+            </div>
+            <input type="submit" value="Add Wine">
+        </form>
+    `
+}
+
